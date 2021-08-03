ed17a5fab8d0b8c89800c45e575f6dbdbe27f1f4
cbahena <82841317+cbahena@users.noreply.github.com>
2021-08-02 22:49:12 -0400

    first commit

---
+++ app/Articulo.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace sisVentas;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Articulo extends Model
+{
+    //
+
+        //
+    protected $table='articulo';
+
+    protected $primaryKey = 'idarticulo';
+
+    public $timestamps = false;
+
+    protected $fillable = ['idarticulo','codigo','nombre', 'stock', 'descripcion', 'imagen', 'estado'];
+
+    protected $guarded = [
+
+    ];
+}
